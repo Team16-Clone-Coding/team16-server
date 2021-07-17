da761095f7e888c02ed2a7a2fcf1af8cb4a8f27a
wonhee77 <leehi402@naver.com>
2021-07-17 10:26:43 +0900

    add : 댓글 작성 기능 추가

--- src/main/java/team16/instagramclone/domain/Comment.java
+++ src/main/java/team16/instagramclone/domain/Comment.java
@@ -1,12 +1,15 @@
 package team16.instagramclone.domain;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import team16.instagramclone.security.UserDetailsImpl;
 import team16.instagramclone.utils.TimeStamped;
 
 import javax.persistence.*;
 
 @Entity
 @Getter
+@NoArgsConstructor
 public class Comment extends TimeStamped {
 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,4 +24,10 @@ public class Comment extends TimeStamped {
 
     @Column(nullable = false)
     private Long postId;
+
+    public Comment(String commentContent, Long postId, String username) {
+        this.commentContent = commentContent;
+        this.postId = postId;
+        this.commentAuthor = username;
+    }
 }
